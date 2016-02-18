a2555e732112d41a0205e2f8fee8214aec2369c0
Luiz Sanches <Luiz Sanches>
2016-02-18 15:03:44 -0200

    controller erro

--- controllers/erroController.php
+++ controllers/erroController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 class erro extends controller {
@@ -13,36 +12,3 @@ class erro extends controller {
 }
 
 ?>
-=======
-<<<<<<< HEAD
-<?php
-
-class erro extends controller {
-
-    public function index_action() {
-        $msg = isset($_SESSION['erro']['msg']) ? $_SESSION['erro']['msg'] : null;
-        $this->smarty->assign('title', 'Oops!');
-        $this->smarty->assign('msg', $msg);
-        $this->smarty->display('erros/index.tpl');
-    }
-
-}
-
-?>
-=======
-<?php
-
-class erro extends controller {
-
-    public function index_action() {
-        $msg = isset($_SESSION['erro']['msg']) ? $_SESSION['erro']['msg'] : null;
-        $this->smarty->assign('title', 'Oops!');
-        $this->smarty->assign('msg', $msg);
-        $this->smarty->display('erros/index.tpl');
-    }
-
-}
-
-?>
->>>>>>> 8521b93384d5547801628c6cf4d9f0edfb17fb57
->>>>>>> 639288d9e5c15f5bef2690fdaa65839ae43926c5
